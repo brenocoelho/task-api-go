9ac5cad63c5ffb7dffa10fea5ca6e01f5e91db5c
Breno <breno.c.coelho@gmail.com>
2020-09-09 12:13:51 -0300

    Add field Path to Tag

--- models/tag.go
+++ models/tag.go
@@ -5,16 +5,18 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm/dialects/postgres"
 )
 
 type Tag struct {
-	UserID    uuid.UUID  `json:"-" gorm:"type:uuid;primary_key"`
-	ID        string     `json:"id" gorm:"type:varchar(36);primary_key"`
-	Name      string     `json:"name" gorm:"type:varchar(100);not null"`
-	Color     string     `json:"color" gorm:"type:varchar(30)"`
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"updated_at"`
-	DeletedAt *time.Time `json:"-" sql:"index"`
+	UserID    uuid.UUID      `json:"-" gorm:"type:uuid;primary_key"`
+	ID        string         `json:"id" gorm:"type:varchar(36);primary_key"`
+	Name      string         `json:"name" gorm:"type:varchar(100);not null"`
+	Color     string         `json:"color" gorm:"type:varchar(30)"`
+	Path      postgres.Jsonb `json:"path"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt *time.Time     `json:"-" sql:"index"`
 }
 
 // BeforeCreate will set a UUID rather than numeric ID.
